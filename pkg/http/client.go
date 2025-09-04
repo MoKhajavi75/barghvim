6b1ee343266e8f718b20d6f45fa0e5cc7591269c
MoKhajavi75 <khajavi.contact@gmail.com>
2025-09-04 10:10:26 +0330

    feat: add http client utility

---
+++ pkg/http/client.go
@@ -0,0 +1,22 @@
+package http
+
+import (
+	"net"
+	"net/http"
+	"time"
+)
+
+var Default = &http.Client{
+	Timeout: 15 * time.Second,
+	Transport: &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   5 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   5 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	},
+}
